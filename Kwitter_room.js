d8336372b1a68f1e780f1a3f1fdda3de5f4acabf
alysharma17 <74836994+alysharma17@users.noreply.github.com>
2021-03-05 12:07:56 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+//Dear firebase, thanks for giving me these links, my css is nice though
+var firebaseConfig = {
+    apiKey: "AIzaSyAKsd2sPGe49t302jbgTjY6TSS_ZuDGfxQ",
+    authDomain: "kwitter-chat-project.firebaseapp.com",
+    projectId: "kwitter-chat-project",
+    storageBucket: "kwitter-chat-project.appspot.com",
+    messagingSenderId: "258991975091",
+    appId: "1:258991975091:web:5084d1f46ec127b86fdec6",
+    measurementId: "G-SGL8CRR8ML"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
